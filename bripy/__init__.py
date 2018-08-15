cecc4309c8e30f9dd95ff04dc3c31dcce2690c8b
The Cheaterman <the.cheaterman@gmail.com>
2018-08-15 22:12:49 +0200

    Make "amount" argument optional again for inc/dec commands

--- bripy/__init__.py
+++ bripy/__init__.py
@@ -148,6 +148,7 @@ def main():
         subparser.add_argument(
             'amount',
             type=float,
+            nargs='?',
             help='Percentage to {}rease brightness by'.format(prefix),
         )
 
