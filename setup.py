e880a68accee98a5b4b9722fab034acf2b35e82c
The Cheaterman <the.cheaterman@gmail.com>
2020-03-03 14:07:51 +0100

    Add url, classifiers, description etc to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
-from setuptools import find_packages, setup
+import pathlib
+import setuptools
 
-setup(
+setuptools.setup(
     name='BrightPy',
     version='0.1.1',
-    packages=find_packages(),
+    author='Cheaterman',
+    author_email='the.cheaterman@gmail.com',
+    description='Control the backlight through sysfs.',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Cheaterman/BriPy',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+    python_requires='>=3.6',
     entry_points={'console_scripts': [
         'bripy = brightpy:main',
         'bripy-ac = brightpy:ac',
         'bripy-battery = brightpy:battery',
     ]},
-    author='Cheaterman',
-    author_email='the.cheaterman@gmail.com',
-    description='Control the backlight through sysfs',
-    license='MIT',
 )
